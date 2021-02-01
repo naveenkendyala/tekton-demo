18785b2cc097dfe4611b91c6bcfc541f83604366
Naveen Kendyala <subscribeken@gmail.com>
2021-02-01 18:37:41 -0500

    added maven build ls task

---
+++ 04.tasks.mvn.build.ls.sh
@@ -0,0 +1,30 @@
+#Black        0;30     Dark Gray     1;30
+#Red          0;31     Light Red     1;31
+#Green        0;32     Light Green   1;32
+#Brown/Orange 0;33     Yellow        1;33
+#Blue         0;34     Light Blue    1;34
+#Purple       0;35     Light Purple  1;35
+#Cyan         0;36     Light Cyan    1;36
+#Light Gray   0;37     White         1;37
+# Scale it back to 0 instance
+
+read -p $'\e[32m[SCRIPT] : Change to tekton-demo project \e[0m: '
+oc project tekton-demo
+echo ""
+
+read -p $'\e[32m[SCRIPT] : Create the Resources \e[0m: '
+oc apply -f demo/resources/resource.build.yaml
+echo ""
+
+read -p $'\e[32m[SCRIPT] : Create the Task with Param and Resource \e[0m: '
+oc apply -f demo/tasks/task.mvn.build.ls.yaml
+echo ""
+
+read -p $'\e[32m[SCRIPT] : List the tasks available in namespace \e[0m: tkn task ls'
+tkn task ls
+echo ""
+
+read -p $'\e[32m[SCRIPT] : Run the Task and observe the output \e[0m: tkn task start mvn-build-ls -p "contextDir=quarkus" -i="source=git-source" --showlog'
+tkn task start mvn-build-ls -p "contextDir=quarkus" -i="source=git-source" --showlog
+echo ""
+
