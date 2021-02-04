eebb52e5b7ab3d4fbad42e232b677e53eee35636
Naveen Kendyala <subscribeken@gmail.com>
2021-02-04 02:39:54 -0500

    added triggers and cleanup script

--- buildah-registry-ocp/quarkus-greeter-advanced/scripts/01.pipeline.build.app.deploy.sh
+++ buildah-registry-ocp/quarkus-greeter-advanced/scripts/01.pipeline.build.app.deploy.sh
@@ -13,7 +13,9 @@ TASKS_DIR=../artifacts/tasks
 RESOURCES_DIR=../artifacts/resources
 UTILS_DIR=../artifacts/utilities
 PIPELINES_DIR=../artifacts/pipelines
+PIPELINERUNS_DIR=../artifacts/pipelineruns
 WORKSPACES_DIR=../artifacts/workspaces
+TRIGGERS_DIR=../artifacts/triggers
 
 
 echo ""
@@ -39,11 +41,33 @@ oc apply -f $WORKSPACES_DIR/shared-space-pvc.yaml
 oc get pvc/shared-space-pvc -o=wide
 echo ""
 
+read -p $'\e[32m[SCRIPT] : Create the PipelineResources \e[0m: '
+oc apply -f $RESOURCES_DIR/resource.build.yaml
+echo ""
 
-#read -p $'\e[32m[SCRIPT] : Expose the service \e[0m: oc expose svc/greeter --port=8080 '
-#oc expose svc/greeter --port=8080
-#echo ""
+read -p $'\e[32m[SCRIPT] : Create the Pipeline \e[0m: '
+oc apply -f $PIPELINES_DIR/pipeline.quarkus.app.deploy.yaml
+echo ""
 
-#read -p $'\e[32m[SCRIPT] : Cleanup \e[0m: '
-#$UTILS_DIR/clean.project.sh
-#echo ""
+read -p $'\e[32m[SCRIPT] : Create the Event Listener, Trigger Binding and Trigger Template \e[0m: '
+oc apply -f $TRIGGERS_DIR/event.listener.github.yaml
+oc apply -f $TRIGGERS_DIR/trigger.binding.yaml
+oc apply -f $TRIGGERS_DIR/trigger.template.yaml
+echo ""
+
+read -p $'\e[32m[SCRIPT] : Expose the Event Listener \e[0m: '
+oc expose svc/el-github-ea-webhook --port=8080
+echo ""
+
+read -p $'\e[32m[SCRIPT] : Execute the PipelineRun \e[0m: '
+oc create -f $PIPELINERUNS_DIR/pipelinerun.quarkus.app.deploy.yaml
+echo ""
+
+read -p $'\e[32m[SCRIPT] : List the Service and expose it \e[0m: oc get svc; oc expose svc/greeter --port=8080 '
+oc get svc
+echo ""
+oc expose svc/greeter --port=8080
+echo ""
+
+read -p $'\e[32m[SCRIPT] Create a Web Hook in the GitHub \e[0m: '
+read -p $'\e[32m[SCRIPT] Make a change in the Source Repository and Test \e[0m: '
